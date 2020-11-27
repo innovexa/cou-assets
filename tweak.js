842583b146834898f52286765895c05778426460
Tony Guo <tony.guo@mail.utoronto.ca>
2020-11-26 19:55:20 -0500

    add assets and minified files for cdn

---
+++ tweak.js
@@ -0,0 +1,49 @@
+function initPage ()  {
+    if(window.location.href.includes("Mode=Edit") || window.location.href.includes("/Forms/") || window.location.href.includes("/Lists/") || window.location.href.includes('_layouts')) 
+    {
+        $('div[data-sp-feature-tag="PageTitle"]').show();
+        $('div[data-automation-id="pageHeader"]').show();
+        $('.headerStyles').hide();
+        
+        $('.commandBarWrapper').css('cssText',
+              "position:relative!important;top:0!important;width:100%;"
+      );
+    }
+    else{
+        $('.commandBarWrapper').css(
+          'cssText',
+          "position: absolute!important;top: 89px!important;width: 100%;"
+        );
+        $('div[class^="headerRow"]').css({
+            position: 'absolute',
+        top:'0px',
+        width:'100%'
+
+        });
+        
+        $('.headerStyles').show();
+        $('.headerStyles').css({
+          
+            'margin-top':'147px'
+        })
+        
+        $('div[data-sp-feature-tag="PageTitle"]').hide();
+        $('div[data-automation-id="pageHeader"]').hide();
+    }
+}
+((history) => {
+    var pushState = history.pushState;
+      history.pushState = (state, key, path) => {
+          pushState.apply(history, [state, key, path]);
+      setTimeout(()=>{
+          initPage();
+  }, 200)
+
+      }
+        
+
+  })(window.history);
+$(document).ready(()=>{
+    initPage();
+});
+
